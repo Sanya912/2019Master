57ddcba4b9369d970efc32048d8a73976c55f35d
Sanya <melikhov912@gmail.com>
2019-07-15 23:08:33 -0400

    regEx1

--- src/practiceJustOneClass/reGex.java
+++ src/practiceJustOneClass/reGex.java
@@ -87,6 +87,18 @@ public class reGex {
 					count++;
 					System.out.println("Occurence " + count + " : " + tNotVMatcher.start() + " to " + tNotVMatcher.end());
 				}
+				
+				//**************************new**************************************
+				String phone1 = "1234567890"; //not
+				String phone2 = "(123) 456-7890"; //yes
+				String phone3 = "123 456-7890"; //not
+				String phone4 = "(123)456-7890";  //not
+				
+				System.out.println("phone1 = " + phone1.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+				System.out.println("phone2 = " + phone2.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$")); //true
+				System.out.println("phone3 = " + phone3.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+				System.out.println("phone4 = " + phone4.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+				
 		
 		
 		

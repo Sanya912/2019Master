d009ada096d91f0b0b2436234d34140c5bea90be
Sanya <melikhov912@gmail.com>
2019-07-21 21:53:57 -0400

    test

--- src/practiceJustOneClass/ComplexNumber.java
+++ src/practiceJustOneClass/ComplexNumber.java
@@ -36,4 +36,7 @@ public class ComplexNumber {
         subtract(complexNumber.real,complexNumber.imaginary);
     }
     ////b///
+    public void leva() {
+    	System.out.println("test");
+    }
 }
